a822852a3c4430f52ab4e5d10e51a28a1da65381
sebha01 <30077548@students.southwales.ac.uk>
2025-04-13 22:18:03 +0100

    updated json files

--- VSC_NESGameTemplate/blank.c
+++ VSC_NESGameTemplate/blank.c
@@ -1,30 +1,81 @@
-/*	simple Hello World, for cc65, for NES
- *  writing to the screen with rendering disabled
- *	using neslib
- *	Doug Fraker 2018
- */	
- 
- 
+/*
+	Code for Nocturnes Blood
+
+	Controller layout 
+
+	Button	|  Bit Value |	neslib Macro
+	A		|	0x80	 |	PAD_A
+	B		|	0x40	 |	PAD_B
+	Select	|	0x20	 |	PAD_SELECT
+	Start	|	0x10	 |	PAD_START
+	Up		|	0x08	 |	PAD_UP
+	Down	|	0x04	 |	PAD_DOWN
+	Left	|	0x02	 |	PAD_LEFT
+	Right	|	0x01	 |	PAD_RIGHT
+
+	Z        ← A Button  
+	X        ← B Button  
+	Enter    ← Start  
+	Right Shift or Tab  ← Select  
+	↑ Arrow  ← Up  
+	↓ Arrow  ← Down  
+	← Arrow  ← Left  
+	→ Arrow  ← Right  
+
+	pad_poll(0) -> reads current state of controller
+	pad_trigger(0) -> returns button once first pressed not held
+	pad_state(0) -> gives you last read value from pad_poll, useful if polled earlier in frame and want to refer to it later
+
+
+	Pro Tip: Add a Simple Pause Menu
+	Here’s a little trick you could build on:
+
+	c
+	if (game_state == STATE_GAME && (pad_trigger(0) & PAD_START)) 
+	{
+		game_state = STATE_PAUSED;
+		// draw pause screen
+	}
+	And later:
+
+	c
+	if (game_state == STATE_PAUSED && (pad_trigger(0) & PAD_START)) 
+	{
+		game_state = STATE_GAME;
+		// resume gameplay
+	}
+
+*/	
  
 #include "LIB/neslib.h"
 #include "LIB/nesdoug.h" 
 
+//Define colours
 #define BLACK 0x0f
 #define DK_GY 0x00
 #define LT_GY 0x10
 #define WHITE 0x30
-// there's some oddities in the palette code, black must be 0x0f, white must be 0x30
- 
+
+//define game states
+#define START_SCREEN 0
+#define GAME_LOOP 1
+#define END_SCREEN 2
+
+
 #pragma bss-name(push, "ZEROPAGE")
 
 // GLOBAL VARIABLES
-// all variables should be global for speed
-// zeropage global is even faster
-
 unsigned char i;
+//Defines which state the game is currently in (START_SCREEN, GAME_LOOP or END_SCREEN)
+unsigned char currentGameState = START_SCREEN;
 
-const unsigned char text[]="Nocturnes Blood"; // zero terminated c string
+const unsigned char text[] = "Nocturnes Blood"; 
+const unsigned char titlePrompt[] = "Press START";
+const unsigned char gameLoopText[] = "This is the game loop";
+const unsigned char endScreenTitle[] = "End Screen";
+const unsigned char endScreenPrompt[] = "To play again";
 
+//palette colours
 const unsigned char palette[]={
 BLACK, DK_GY, LT_GY, WHITE,
 0,0,0,0,
@@ -32,31 +83,79 @@ BLACK, DK_GY, LT_GY, WHITE,
 0,0,0,0
 }; 
 
+//function prototypes
+void DrawTitleScreen(void);
+void GameLoop(void);
+void Fade(void);
 
-void main (void) {
-	
-	ppu_off(); // screen off
-	pal_bg(palette); //	load the BG palette
+void main (void) 
+{
+	DrawTitleScreen();
 
-	//Set VRAM address to row 10 and column 12
-	vram_adr(NTADR_A(8, 8)); // places text at screen position
-	vram_write(text, sizeof(text)-1); //write Title to screen
+	// infinite loop
+	while (1)
+	{
+		//Waits for next frame
+		ppu_wait_nmi();
+		pad_poll(0);       // poll the controller
 
-	// vram_adr and vram_put only work with screen off
-	// NOTE, you could replace everything between i = 0; and here with...
-	// vram_write(text,sizeof(text));
-	// does the same thing
-	ppu_on_all(); //	turn on screen
-	
-	
-	while (1){
-		// infinite loop
-		// game code can go here later.
-		
-		pal_fade_to(0,4); // fade from black to normal
-		delay(50);
-		pal_fade_to(4,0); // fade from normal to black
-		delay(50);
+		if (pad_trigger(0) & PAD_START)
+		{
+			currentGameState = GAME_LOOP;
+			GameLoop();
+		}
+
+		switch(currentGameState)
+		{
+			case START_SCREEN:
+				//Check if player has pressed start
+				Fade();
+				//DrawTitleScreen();
+				break;
+			case GAME_LOOP:
+				Fade();
+				break;
+		}
 	}
 }
 	
+
+void DrawTitleScreen(void)
+{
+	ppu_off(); // screen off
+	pal_bg(palette); //	load the BG palette
+
+	// vram_adr and vram_put only work with screen off NOTE, you could replace everything between i = 0; and here with...
+	// vram_write(text,sizeof(text)); does the same thing
+
+	//Set VRAM address to row 10 and column 12
+	vram_adr(NTADR_A(8, 8)); // places text at screen position
+	vram_write(text, sizeof(text) - 1); //write Title to screen
+
+	vram_adr(NTADR_A(3, 6));
+	vram_write(titlePrompt, sizeof(titlePrompt) - 1);
+
+	ppu_on_all(); //	turn on screen
+}
+
+void GameLoop(void)
+{
+	//Turn screen off
+	ppu_off(); 
+	//Load palette
+	pal_bg(palette);
+
+	//Set VRAM ADDRESS TO ROW 10 COL 10
+	vram_adr(NTADR_A(10, 10));
+	vram_write(gameLoopText, sizeof(gameLoopText) - 1);
+
+	ppu_on_all();
+}
+
+void Fade(void)
+{
+	pal_fade_to(0,4); // fade from black to normal
+	delay(50);
+	pal_fade_to(4,0); // fade from normal to black
+	delay(50);
+}
--- VSC_NESGameTemplate/blank.s
+++ VSC_NESGameTemplate/blank.s
@@ -12,21 +12,46 @@
 	.macpack	longbranch
 	.forceimport	__STARTUP__
 	.import		_pal_bg
+	.import		_ppu_wait_nmi
 	.import		_ppu_off
 	.import		_ppu_on_all
+	.import		_pad_poll
+	.import		_pad_trigger
 	.import		_vram_adr
 	.import		_vram_write
 	.import		_delay
 	.import		_pal_fade_to
 	.export		_i
+	.export		_currentGameState
 	.export		_text
+	.export		_titlePrompt
+	.export		_gameLoopText
+	.export		_endScreenTitle
+	.export		_endScreenPrompt
 	.export		_palette
+	.export		_DrawTitleScreen
+	.export		_GameLoop
+	.export		_Fade
 	.export		_main
+
+.segment	"DATA"
+
+_currentGameState:
+	.byte	$00
 
 .segment	"RODATA"
 
 _text:
 	.byte	$4E,$6F,$63,$74,$75,$72,$6E,$65,$73,$20,$42,$6C,$6F,$6F,$64,$00
+_titlePrompt:
+	.byte	$50,$72,$65,$73,$73,$20,$53,$54,$41,$52,$54,$00
+_gameLoopText:
+	.byte	$54,$68,$69,$73,$20,$69,$73,$20,$74,$68,$65,$20,$67,$61,$6D,$65
+	.byte	$20,$6C,$6F,$6F,$70,$00
+_endScreenTitle:
+	.byte	$45,$6E,$64,$20,$53,$63,$72,$65,$65,$6E,$00
+_endScreenPrompt:
+	.byte	$54,$6F,$20,$70,$6C,$61,$79,$20,$61,$67,$61,$69,$6E,$00
 _palette:
 	.byte	$0f
 	.byte	$00
@@ -52,12 +77,12 @@ _i:
 	.res	1,$00
 
 ; ---------------------------------------------------------------
-; void __near__ main (void)
+; void __near__ DrawTitleScreen (void)
 ; ---------------------------------------------------------------
 
 .segment	"CODE"
 
-.proc	_main: near
+.proc	_DrawTitleScreen: near
 
 .segment	"CODE"
 
@@ -78,7 +103,7 @@ _i:
 	lda     #$08
 	jsr     _vram_adr
 ;
-; vram_write(text, sizeof(text)-1); //write Title to screen
+; vram_write(text, sizeof(text) - 1); //write Title to screen
 ;
 	lda     #<(_text)
 	ldx     #>(_text)
@@ -87,13 +112,83 @@ _i:
 	lda     #$0F
 	jsr     _vram_write
 ;
+; vram_adr(NTADR_A(3, 6));
+;
+	ldx     #$20
+	lda     #$C3
+	jsr     _vram_adr
+;
+; vram_write(titlePrompt, sizeof(titlePrompt) - 1);
+;
+	lda     #<(_titlePrompt)
+	ldx     #>(_titlePrompt)
+	jsr     pushax
+	ldx     #$00
+	lda     #$0B
+	jsr     _vram_write
+;
 ; ppu_on_all(); // turn on screen
 ;
-	jsr     _ppu_on_all
+	jmp     _ppu_on_all
+
+.endproc
+
+; ---------------------------------------------------------------
+; void __near__ GameLoop (void)
+; ---------------------------------------------------------------
+
+.segment	"CODE"
+
+.proc	_GameLoop: near
+
+.segment	"CODE"
+
+;
+; ppu_off(); 
+;
+	jsr     _ppu_off
+;
+; pal_bg(palette);
+;
+	lda     #<(_palette)
+	ldx     #>(_palette)
+	jsr     _pal_bg
+;
+; vram_adr(NTADR_A(10, 10));
+;
+	ldx     #$21
+	lda     #$4A
+	jsr     _vram_adr
+;
+; vram_write(gameLoopText, sizeof(gameLoopText) - 1);
+;
+	lda     #<(_gameLoopText)
+	ldx     #>(_gameLoopText)
+	jsr     pushax
+	ldx     #$00
+	lda     #$15
+	jsr     _vram_write
+;
+; ppu_on_all();
+;
+	jmp     _ppu_on_all
+
+.endproc
+
+; ---------------------------------------------------------------
+; void __near__ Fade (void)
+; ---------------------------------------------------------------
+
+.segment	"CODE"
+
+.proc	_Fade: near
+
+.segment	"CODE"
+
 ;
 ; pal_fade_to(0,4); // fade from black to normal
 ;
-L0002:	lda     #$00
+	lda     #$00
 	jsr     pusha
 	lda     #$04
 	jsr     _pal_fade_to
@@ -113,9 +208,74 @@ L0002:	lda     #$00
 ; delay(50);
 ;
 	lda     #$32
-	jsr     _delay
+	jmp     _delay
+
+.endproc
+
+; ---------------------------------------------------------------
+; void __near__ main (void)
+; ---------------------------------------------------------------
+
+.segment	"CODE"
+
+.proc	_main: near
+
+.segment	"CODE"
+
 ;
-; while (1){
+; DrawTitleScreen();
+;
+	jsr     _DrawTitleScreen
+;
+; ppu_wait_nmi();
+;
+L0002:	jsr     _ppu_wait_nmi
+;
+; pad_poll(0);       // poll the controller
+;
+	lda     #$00
+	jsr     _pad_poll
+;
+; if (pad_trigger(0) & PAD_START)
+;
+	lda     #$00
+	jsr     _pad_trigger
+	and     #$10
+	beq     L000A
+;
+; currentGameState = GAME_LOOP;
+;
+	lda     #$01
+	sta     _currentGameState
+;
+; GameLoop();
+;
+	jsr     _GameLoop
+;
+; switch(currentGameState)
+;
+L000A:	lda     _currentGameState
+;
+; }
+;
+	beq     L0008
+	cmp     #$01
+	beq     L0009
+	jmp     L0002
+;
+; Fade();
+;
+L0008:	jsr     _Fade
+;
+; break;
+;
+	jmp     L0002
+;
+; Fade();
+;
+L0009:	jsr     _Fade
+;
+; break;
 ;
 	jmp     L0002
 
